593c54362670f9b8fa9d6dfd4e55495ca2c098f8
Shubhamsingh123lpu <159036530+Shubhamsingh123lpu@users.noreply.github.com>
2024-02-21 22:35:03 +0530

    Update Bub.cpp

--- Bub.cpp
+++ Bub.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+#include <wingdi.h>
+using namespace std;
+int input (int num[],int size)
+{
+    for(int i=0;i<(size-1);i++)
+    {
+for(int j=0;j<(size-1-i);j++){
+          if(num[j]>num[j+1])
+          {
+            swap(num[j],num[j+1]);
+          }
+    }
+}
+}
+void print(int num[],int n){
+for(int k=0;k<n;k++){
+cout<<num[k]<<" ";
+}
 
+}
+int main(){
+    int size;
+      cout<<"enter the number : "<<endl;
+    cin>>size;
+    int num[100];
+    for(int i=0;i<size;i++){
+        cin>>num[i];
+    }
+    input(num,size);
+    print(num,size);
+}
